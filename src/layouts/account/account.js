377d5935c98b3a114cbe1a8c8f257bff7b50ae4b
Bohdan Benkevych <70970651+petryshynm@users.noreply.github.com>
2023-04-10 21:12:56 +0300

    Added tab routing on account page

--- src/layouts/account/account.js
+++ src/layouts/account/account.js
@@ -1,6 +1,6 @@
 import { Tab, Tabs} from "@mui/material";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { MyAgreements } from "./MyAgreements";
 import { SignAgreements } from "./SignAgreements";
@@ -13,37 +13,43 @@ const pages = [
   {
     component: <MyAgreements/>,
     label: "My agreements",
+    id: 'my-agreements',
   },
   {
     component: <SignAgreements/>,
     label: "Sign agreements",
+    id: 'sign-agreements',
   },
   {
     component: <Profile/>,
     label: "Profile",
+    id: 'profile',
   },
   {
     component: <Templates/>,
     label: "Templates",
+    id: 'templates',
   }
 ] 
 
 const TabsPanel = ({ value }) => pages[value].component
 
 export const Account = () => {
-  const [tabIndex, setTabIndex] = useState(0);
   const { tab } = useParams();
+  const paramsTabIndex = pages.findIndex((t) => t.id === tab);
+  const tabIndex = paramsTabIndex !== -1 ? paramsTabIndex : 0;
+  const navigate = useNavigate();
   // const createFile = async (title) => {
   //   dispatch(createDocumentRequest());
   // };
-  console.log(tab);
+
   return (
     <div className="account">
       <Tabs
         orientation="vertical"
         variant="scrollable"
         value={tabIndex}
-        onChange={(e, newIndex) => setTabIndex(newIndex)}
+        onChange={(e, newIndex) => navigate(`/account/${pages[newIndex].id}`)}
         sx={{ borderRight: 1, borderColor: 'divider' }}
       >
         {pages.map(({label}) => <Tab key={label} label={label}/>)}
